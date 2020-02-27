ec8b07b9b7ff3a4f32246a2c82c45e95f66d6ab1
NewWeiNern <nwnpoly@gmail.com>
2020-02-27 19:25:17 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,40 +1,37 @@
-$(".page" + window.location.hash).addClass("page-active");
-$("a[href='" + (window.location.hash == "" ? "#profile" : window.location.hash) + "']").addClass("nav-active");
+const web_data = {
+    previous : null,
+    current : (window.location.hash || "#profile")
+};
+
+
+$(`${web_data.current}.page`).addClass("page-active");
+$(`a[href="${web_data.current}"`).addClass("nav-active");
 
 $(".navbar li a").click(function(){
+    // set to the correct page
     let current = $(this).attr("href");
+
+    web_data.previous = web_data.current;
+    web_data.current = current;
+
+    if(web_data.current !== web_data.previous){
+        switch(web_data.current){
+            case "#profile" : 
+                $(web_data.current)
+                .css({"left" : "-100%"})
+                .animate({"left": 0});
+            break;
+            case "#location" : 
+                $(web_data.current).css({left : 1000})
+            break;
+        }
+    }
+
     $(".page.page-active").removeClass("page-active");
     $(".page" + current).addClass("page-active");
     $(".nav-active").removeClass("nav-active");
     $(this).addClass("nav-active");
+
 });
 
-// const vid = {
-//     element : document.querySelector("#cam_vid"),
-//     options : initVideoObjectOptions("cam_vid"),
-//     id : 0,
-//     init(){
-//         console.log(this.options);
-//         initScanner(this.options);
-//         initAvaliableCameras($("#select"), (e)=>{
-//             alert(e);
-//             this.id =0;
-//         })
-//         initCamera(this.id);
-//         scanStart(function(data){
-//             alert(data);
-//         });
-
-//         this.element.addEventListener("play", function(){
-//             var $this = this;
-
-//             (function loop(){
-//                 if(!$this.paused && !$this.ended){
-//                     setTimeout(loop, 1000/30);
-//                 }
-//             })();
-//         }, 0);
-//     }
-// };
-
-// vid.init();
+$("#image-input").on("change", scanner);
---
+++ js/scanner.js
@@ -0,0 +1,17 @@
+function scanner(e){
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    const $this = this;
+    reader.onloadend = function(e){
+        qrcode.decode(reader.result);
+    }
+    qrcode.callback = function(res){
+        if(res instanceof Error){
+            alert("QR Code is not found.");
+        }
+        else{
+            alert(res);
+        }
+    }
+    reader.readAsDataURL(file);
+}
